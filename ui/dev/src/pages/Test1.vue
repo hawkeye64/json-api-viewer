ee60dcea619631bccc201f73773cace56462fe8d
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-07-12 07:28:04 -0600

    chore: misc updates

--- ui/dev/src/pages/Test1.vue
+++ ui/dev/src/pages/Test1.vue
@@ -1,17 +1,20 @@
 <template>
   <q-page padding>
     <json-api-viewer title="QCalendar API" :json="calendarApi" />
+    <json-api-viewer title="QCalendar Exports" :json="timestampApi" type="Exports" starting-tab="methods" />
     <json-api-viewer title="QMediaPlayer API" :json="mediaPlayerApi" />
   </q-page>
 </template>
 
 <script>
 import calendarApi from '../api/QCalendar.json'
+import timestampApi from '../api/timestamp.json'
 import mediaPlayerApi from '../api/QMediaPlayer.json'
 export default {
   data () {
     return {
       calendarApi,
+      timestampApi,
       mediaPlayerApi
     }
   }
